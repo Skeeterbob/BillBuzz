4c3a870c54a08cccd5f4f38ae3ead3dd2398c5dd
Raigene <98432844+Raigene@users.noreply.github.com>
2023-09-17 16:40:50 +0000

    Initial Server Setup

--- server/app.js
+++ server/app.js
@@ -2,6 +2,17 @@ import dotenv from 'dotenv';
 dotenv.config('../.env');
 import {TwilioHandler} from './twilioHandler.js';
 const twilioHandler = new TwilioHandler();
-await twilioHandler.init().then(() => {
-    twilioHandler.sendSMS(process.env.TEST_PHONE);
-});
+const PORT = 4000;
+const app = express();
+const express = require('express');
+const mongoose = require("mongoose")
+//Connect to mongoDB
+mongoose.connect('mongodb+srv://eg0547:6GeKD4a76qhbSzb@wsucluster0.3pqwigj.mongodb.net/?retrywrites=true&w=majority');
+
+app.listen(PORT, function(err) {
+   if(err) console.log(`Server NOT connected!`)
+    console.log(`Server started on port ${PORT}`);
+  });
+
+
+
