5d4119bb1954c310c27257cf057e5c7d9fd7742e
Ghaddar <hadimghaddar@gmail.com>
2023-09-17 21:22:03 -0400

    Fix imports without quotes

--- server/routes/routers/dBRouter.js
+++ server/routes/routers/dBRouter.js
@@ -1,4 +1,4 @@
-import express from express;
+import express from 'express';
 const dBRouter = express.Router();
 
 export {dBRouter};
--- server/routes/routers/plaidRouter.js
+++ server/routes/routers/plaidRouter.js
@@ -1,4 +1,4 @@
-import express from express;
+import express from 'express';
 const plaidRouter = express.Router();
 
 export {plaidRouter};
