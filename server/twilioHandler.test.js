acea0bd0612d854f66a024722f2a8b64a4b9af01
Bryan Hodgins <bryanh@the-extractory.com>
2023-09-20 09:51:04 -0400

    TwilioHandler Testing and async changes

--- server/twilioHandler.test.js
+++ server/twilioHandler.test.js
@@ -8,30 +8,27 @@ require('dotenv').config();
 
 
 test('sending SMS code and verify code', async () => {
-    //create an interface for input and output operations
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    })
+    
+    
     const twilioHandler = new TwilioHandler();
     await twilioHandler.init().then(async () =>{
         console.log(process.env.TEST_PHONE);
         await twilioHandler.sendSMS(process.env.TEST_PHONE).then(async verification => {
-            //NEED TO ADD THE USER INPUT HERE TO RUN VALIDATION OF SMS CODE!********************************
-            let userInput = "";
+            expect(await verification.status).toBe("pending");
+        })
+    })   
+}, 10000)
+
+//create an interface for input and output operations
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+let userInput = "";
             rl.question("Enter Verification Code: ", function(string){
                 console.log(string);
                 userInput = string;
                 rl.close();
             })
-            console.log(userInput, '2');
-            await twilioHandler.validateSMSCode(process.env.TEST_PHONE, userInput).then(async response => {
-                console.log(response);
-                expect(await response).toBe(true);
-            });            
-        })
-    })   
-}, 120000)
-
     
     
