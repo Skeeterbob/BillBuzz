ea784d8bcc939490401428041497db91b51c4c43
Bryan Hodgins <bryanh@the-extractory.com>
2023-09-19 23:58:46 -0400

    twilioHandler work

--- server/twilioHandler.js
+++ server/twilioHandler.js
@@ -6,34 +6,47 @@ import Twilio from 'twilio';
 const client = Twilio(accountSid, authToken);
 
 
+
 class TwilioHandler {
     #serviceSid;
-    async init () {
-        await client.verify.v2.services
+    init () {
+        return new Promise((resolve, reject) => {
+            client.verify.v2.services
             .create({friendlyName: 'BillBuzz service'})
-            .then(service => {this.#serviceSid = service.sid; return});
+            .then(service => {this.#serviceSid = service.sid; 
+                console.log(this.#serviceSid);
+                resolve();
+            });
+        })
     }
     sendSMS (phNum) {
-        console.log(this.#serviceSid, 'inSMS')
-        client.verify.v2.services(this.#serviceSid)
-            .verifications
-            .create({to: phNum, channel: 'sms'})
-            .then(verification => console.log(verification.status));
+        return new Promise((resolve, reject) => {
+            console.log(this.#serviceSid, 'inSMS')
+            client.verify.v2.services(this.#serviceSid)
+                .verifications
+                .create({to: phNum, channel: 'sms'})
+                .then(async verification => {
+                    console.log(verification.status);
+                    resolve(verification);
+                });     
+        })             
     }
     //function to return true for a positive validation of the SMS code
     //or false for an invalid sms code entry
     validateSMSCode (phNum, code) {
-        client.verify.v2.services(this.#serviceSid)
-            .verificationChecks
-            .create({to: phNum, code: code})
-            .then(verification_check => {
-                if(verification_check.status == "approved"){
-                    return true;
-                }
-                else {
-                    return false;
-                }
-            });
+        return new Promise((resolve, reject) => {
+            client.verify.v2.services(this.#serviceSid)
+                .verificationChecks
+                .create({to: phNum, code: code})
+                .then(verification_check => {
+                    if(verification_check.status == "approved"){
+                        resolve(true);
+                    }
+                    else {
+                        reject(false);
+                    }
+                });
+        });
     }
 }
 
--- server/twilioHandler.test.js
+++ server/twilioHandler.test.js
@@ -1,6 +1,37 @@
 const {TwilioHandler} = require('./twilioHandler.js');
-const twilioHandler = new TwilioHandler();
+const readline = require('readline')
+
+//load environment variables from .env
 require('dotenv').config();
-test('checking process.env', () => {
-    expect(twilioHandler.testFunc(1)).toBe(1);
-});
+
+
+
+
+test('sending SMS code and verify code', async () => {
+    //create an interface for input and output operations
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
+    const twilioHandler = new TwilioHandler();
+    await twilioHandler.init().then(async () =>{
+        console.log(process.env.TEST_PHONE);
+        await twilioHandler.sendSMS(process.env.TEST_PHONE).then(async verification => {
+            //NEED TO ADD THE USER INPUT HERE TO RUN VALIDATION OF SMS CODE!********************************
+            let userInput = "";
+            rl.question("Enter Verification Code: ", function(string){
+                console.log(string);
+                userInput = string;
+                rl.close();
+            })
+            console.log(userInput, '2');
+            await twilioHandler.validateSMSCode(process.env.TEST_PHONE, userInput).then(async response => {
+                console.log(response);
+                expect(await response).toBe(true);
+            });            
+        })
+    })   
+}, 120000)
+
+    
+    
