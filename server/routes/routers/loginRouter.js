46c18ef50cfbd1b04952605451159793579818e2
efhill2 <43968642+efhill2@users.noreply.github.com>
2023-09-27 20:57:11 -0400

    Revert "Changes to /login/verify/sms route"
    This reverts commit 3ca77fb40611492a71545fc08b66fcbb9d4cede2.

--- server/routes/routers/loginRouter.js
+++ server/routes/routers/loginRouter.js
@@ -8,23 +8,52 @@ const dBHandler =  new DBHandler();
 const twilioHandler = new TwilioHandler();
 
 twilioHandler.init();
+dBHandler.init();
 
-//Get request for login through Twilio
-loginRouter.post('/', async(req, res)=>
-{
- try{
- 
-    //Request users' phone number
-    const phNum = req.body.phNum;
-    const sendSMS = sendSMS();
- }
- catch(err){
-    console.err(error);
-    res.status(401).json({error:'Login Error'});
- }
+loginRouter.post('/login', async (req, res) => {
 });
 
+loginRouter.post('/login/verify', async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
 
+    if (!email || !password) {
+        return res.status(400).send('Email and Password fields are required');
+    }
 
+    const result = await dBHandler.verifyUser(email, password);
+    
+    if (result) {
+        return res.status(200).send(JSON.stringify(result));
+    }else {
+        return res.status(401).send('Incorrect Email/Password provided!');
+    }
+});
+
+loginRouter.post('/login/verify/sms', async(req,res)=>{
+    try{
+        const phNum = req.body.phNum;
+        const code = req.body.code;
+        const token = req.body.token;
+        const id = req.body.id;
+
+        const ctoken = AuthHandler.createToken(token, id);
+        if(!ctoken){
+            return res.status(400).json({error:'TOKEN INVALID!'});
+        }
+
+        const result = await twilioHandler.validateSMSCode('+1'+ phNum, code);
+        if(result){
+            res.status(200).send(JSON.stringify(result));
+        }
+        else{
+            res.status(400).json({error:'Verification Error!'});
+        }
+    }
+        catch(error){
+            console.error(error);
+            res.status(500).json({error:'Error'});
+        }
+    });
 
 export {loginRouter};
