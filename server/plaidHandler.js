8416676ab175066c17f40809416803b20fa0025f
efhill2 <43968642+efhill2@users.noreply.github.com>
2023-10-14 18:31:32 -0400

    deleteAccount method added

--- server/plaidHandler.js
+++ server/plaidHandler.js
@@ -80,7 +80,7 @@ class PlaidHandler {
                 itemId: response.data.item_id}
         }
         catch (error) {
-            // handle error using PLaidAPI specific error handeling
+            // handle error using PlaidAPI specific error handeling
             if(error.response && error.response.data){
                 const plaidError = error.response.data;
 
@@ -92,7 +92,20 @@ class PlaidHandler {
             }
             console.error('Plaid completeLink error:', error);
 
-                return{error:'An error occurred while completing the link process.'};
+                return{error:'An error occurred while completing the link process.'};      
+        }
+    }
+//Method to delete the plaid account
+    async deleteAccount(accessToken){
+        try{
+            const response = await this.#client.itemRemove({
+                access_token: accessToken,
+            });
+            return response.data;
+        }
+        catch(error){
+            console.error('Plaid deleteAccount error:', error);
+            throw error;
         }
     }
 };
