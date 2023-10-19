bdf33ee1848bae25a5cd9610815b65d8c6ae94bd
Ghaddar <hadimghaddar@gmail.com>
2023-10-18 20:51:50 -0400

    Added null checks to create user route

--- server/routes/routers/registerRouter.js
+++ server/routes/routers/registerRouter.js
@@ -6,10 +6,14 @@ const registerRouter = express.Router();
 
 registerRouter.post('/createUser', async (req, res) => {
     try {
-        let user = new User({...req.body});
-        const result = await dbHandler.insertUser(user);
-        await twilioHandler.sendSMS('+1' + user.getPhoneNumber());
-        res.status(200).json(result);
+        if (req.body.user) {
+            let user = new User({...req.body.user});
+            const result = await dbHandler.insertUser(user);
+            await twilioHandler.sendSMS('+1' + user.getPhoneNumber());
+            res.status(200).json(result);
+        }else {
+            res.status(400).json({error: 'no user data specified.'});
+        }
     } catch (err) {
         console.log(err);
         res.status(400).json({error: "/register/createUser endpoint error"})
